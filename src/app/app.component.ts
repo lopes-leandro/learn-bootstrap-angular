466777716a99a0911a62faf1d42bb2360518449e
Leandro Lopes <leandro.lopes@confitec.com.br>
2019-10-28 13:00:11 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bsa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'learn-bootstrap-angular';
+}
